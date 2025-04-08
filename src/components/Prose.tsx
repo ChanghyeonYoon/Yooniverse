929b2695ea214514ba7044b7cccb4f0265f80ca3
Changhyeon Yoon <yoon@changhyeon.net>
2025-04-08 17:49:49 +0900

    Fix : Text Color

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -11,7 +11,7 @@ export function Prose({
     <div
       className={clsx(
         className,
-        "prose prose-sm prose-blue max-w-none prose-p:text-secondary prose-headings:text-primary"
+        "prose prose-sm prose-blue max-w-none prose-p:text-primary prose-headings:text-primary"
       )}
     >
       {children}
